e6b39b679d317b0a85f5d1d75f31d22a3fc698a1
Shane Johnson <ShaneBrandonJohnson@gmail.com>
2025-11-26 17:54:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "evolving-chess",
+  "appId": "1:516146964471:web:1cf584147078f328e630f2",
+  "apiKey": "AIzaSyDhCEZLnsgT-BMByMoGmznToSLNQhhtpVI",
+  "authDomain": "evolving-chess.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "516146964471"
+};
