d8a3ba798e1bc89786b75745d754ab74b8f6e36b
Shane Johnson <ShaneBrandonJohnson@gmail.com>
2026-09-19 17:34:27 +0000

    I don't see the 3 newest items in the loot bag for playtesting.  Fix thi

--- src/firebase/auth/use-user.tsx
+++ src/firebase/auth/use-user.tsx
@@ -41,6 +41,7 @@ interface UserData {
   lootSyncV5?: boolean;
   lootSyncV6?: boolean;
   lootSyncV7?: boolean;
+  lootSyncV8?: boolean;
 }
 
 const ITEM_TYPES = Object.keys(ITEM_METADATA) as InventoryItemType[];
@@ -137,7 +138,8 @@ export function useUser() {
             lootSyncV4: true,
             lootSyncV5: true,
             lootSyncV6: true,
-            lootSyncV7: true
+            lootSyncV7: true,
+            lootSyncV8: true
           };
         } else {
           currentData = snap.data() as UserData;
@@ -159,9 +161,9 @@ export function useUser() {
           return { type, count };
         });
 
-        // Forced sync for Batch 7 Playtesting (Weighted Helm, Crowbar, Signal Horn)
-        if (!currentData.lootSyncV7) {
-            const forceAdd: InventoryItemType[] = ['weighted_helm', 'crowbar', 'signal_horn'];
+        // Forced sync for Batch 8 Playtesting (Weighted Helm, Crowbar, Signal Horn, Antifreeze, Frayed Rope)
+        if (!currentData.lootSyncV8) {
+            const forceAdd: InventoryItemType[] = ['weighted_helm', 'crowbar', 'signal_horn', 'antifreeze', 'frayed_rope', 'coffee_bean', 'filter_mask', 'thermal_socks', 'spiked_buckler', 'whetstone', 'grip_gloves', 'clover'];
             forceAdd.forEach(t => {
                 const itemIdx = updatedInventory.findIndex(i => i.type === t);
                 if (itemIdx > -1) {
@@ -174,7 +176,7 @@ export function useUser() {
                     inventoryNeedsSync = true;
                 }
             });
-            updates.lootSyncV7 = true;
+            updates.lootSyncV8 = true;
             needsUpdate = true;
         }
 
